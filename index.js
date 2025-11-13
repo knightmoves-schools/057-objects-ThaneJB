5541823db1808d795a02d29241b812ca7c5bc538
Thane <thabay0606@students.maquoketaschools.org>
2025-11-13 14:03:36 -0600

    Removed period

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ let school = {
   'mascot': 'Tiger',
 };
 
-let description = `${school.name} has an average class size of ${school['average class size']} and their mascot is a ${school['mascot']}.`
+let description = `${school.name} has an average class size of ${school['average class size']} and their mascot is a ${school['mascot']}`
 
 document.getElementById('result').innerHTML = description;
+
 
